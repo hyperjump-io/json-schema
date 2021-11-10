b964f10a5f986a5fb3acdc3360f204866de763a4
Jason Desrosiers <jdesrosi@gmail.com>
2021-11-09 20:02:40 -0800

    Add non-default types for index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,6 +17,18 @@ export type JsonSchema = {
   InvalidSchemaError: typeof InvalidSchemaError;
 };
 
+export const add: JsonSchema["add"];
+export const get: JsonSchema["get"];
+export const validate: JsonSchema["validate"];
+export const compile: JsonSchema["compile"];
+export const interpret: JsonSchema["interpret"];
+export const setMetaOutputFormat: JsonSchema["setMetaOutputFormat"];
+export const setShouldMetaValidate: JsonSchema["setShouldMetaValidate"];
+export const FLAG: JsonSchema["FLAG"];
+export const BASIC: JsonSchema["BASIC"];
+export const DETAILED: JsonSchema["DETAILED"];
+export const VERBOSE: JsonSchema["VERBOSE"];
+
 export * from "@hyperjump/json-schema-core";
 export * from "./draft-04";
 export * from "./draft-06";
