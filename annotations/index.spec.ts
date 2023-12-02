7d1fde8e4375a659d68ba8c83766b401b4d6bf95
Jason Desrosiers <jdesrosi@gmail.com>
2023-12-01 16:44:34 -0800

    Fix annotation bug

--- annotations/index.spec.ts
+++ annotations/index.spec.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import { expect } from "chai";
+import { toAbsoluteIri } from "@hyperjump/uri";
 
 import * as AnnotatedInstance from "./annotated-instance.js";
 import { annotate } from "./index.js";
@@ -42,40 +43,42 @@ const host = "https://annotations.json-schema.hyperjump.io";
 
 const testSuiteFilePath = `${__dirname}/tests`;
 
-fs.readdirSync(testSuiteFilePath, { withFileTypes: true })
-  .filter((entry) => entry.isFile() && entry.name.endsWith(".json"))
-  .forEach((entry) => {
-    const file = `${testSuiteFilePath}/${entry.name}`;
-    const suites = JSON.parse(fs.readFileSync(file, "utf8")) as Suite[];
+describe("Annotations", () => {
+  fs.readdirSync(testSuiteFilePath, { withFileTypes: true })
+    .filter((entry) => entry.isFile() && entry.name.endsWith(".json"))
+    .forEach((entry) => {
+      const file = `${testSuiteFilePath}/${entry.name}`;
+      const suites = JSON.parse(fs.readFileSync(file, "utf8")) as Suite[];
 
-    suites.forEach((suite) => {
-      describe(suite.title + "\n" + JSON.stringify(suite.schema, null, "  "), () => {
-        let annotator: Annotator;
+      suites.forEach((suite) => {
+        describe(suite.title + "\n" + JSON.stringify(suite.schema, null, "  "), () => {
+          let annotator: Annotator;
 
-        beforeEach(async () => {
-          const id = `${host}/${encodeURIComponent(suite.title)}`;
-          addSchema(suite.schema, id, dialectId);
+          beforeEach(async () => {
+            const id = `${host}/${encodeURIComponent(suite.title)}`;
+            addSchema(suite.schema, id, dialectId);
 
-          annotator = await annotate(id);
-        });
+            annotator = await annotate(id);
+          });
 
-        suite.subjects.forEach((subject) => {
-          describe("Instance: " + JSON.stringify(subject.instance), () => {
-            let instance: AnnotatedJsonDocument;
+          suite.subjects.forEach((subject) => {
+            describe("Instance: " + JSON.stringify(subject.instance), () => {
+              let instance: AnnotatedJsonDocument;
 
-            beforeEach(() => {
-              instance = annotator(subject.instance);
-            });
+              beforeEach(() => {
+                instance = annotator(subject.instance);
+              });
 
-            subject.assertions.forEach((assertion) => {
-              it(`${assertion.keyword} annotations at '${assertion.location}' should be ${JSON.stringify(assertion.expected)}`, () => {
-                const dialect: string = suite.schema.$schema as string;
-                const annotations = AnnotatedInstance.annotation(AnnotatedInstance.get(assertion.location, instance), assertion.keyword, dialect);
-                expect(annotations).to.eql(assertion.expected);
+              subject.assertions.forEach((assertion) => {
+                it(`${assertion.keyword} annotations at '${assertion.location}' should be ${JSON.stringify(assertion.expected)}`, () => {
+                  const dialect: string | undefined = suite.schema.$schema ? toAbsoluteIri(suite.schema.$schema as string) : undefined;
+                  const annotations = AnnotatedInstance.annotation(AnnotatedInstance.get(assertion.location, instance), assertion.keyword, dialect);
+                  expect(annotations).to.eql(assertion.expected);
+                });
               });
             });
           });
         });
       });
     });
-  });
+});
