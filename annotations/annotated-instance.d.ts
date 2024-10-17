5bb8f4c3d09beb7ff2ba1078319017885d792c75
Jason Desrosiers <jdesrosi@gmail.com>
2024-10-17 16:55:33 -0700

    Update annotation types

--- annotations/annotated-instance.d.ts
+++ annotations/annotated-instance.d.ts
@@ -1,5 +1,5 @@
 export const setAnnotation: (keywordUri: string, schemaLocation: string, value: string) => void;
 export const annotation: <A>(instance: JsonNode, keyword: string, dialectUri?: string) => A[];
-export const annotatedWith: (instance: JsonNode, keyword: string, dialectUri?: string) => JsonNode[];
+export const annotatedWith: <A extends JsonNode>(instance: A, keyword: string, dialectUri?: string) => A[];
 
 export * from "../lib/instance.js";
